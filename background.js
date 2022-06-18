65272a5d28c41d2e0c7a5a79c39dffb22f8bacc1
Yusuf Hilmi <177yusuf177@gmail.com>
2022-06-18 19:56:31 +0300

    add redirect through messages api

--- background.js
+++ background.js
@@ -26,3 +26,9 @@ chrome.storage.onChanged.addListener(function (changes, namespace) {
     );
   }
 });
+
+chrome.runtime.onMessage.addListener(function (request, sender) {
+  console.log(sender.tab.id);
+  console.log(request);
+  chrome.tabs.update(sender.tab.id, { url: request.redirect });
+});
--- content.js
+++ content.js
@@ -18,6 +18,30 @@ let domains;
 // get the hostname
 const hostname = window.location.hostname;
 
+/* if we were to go with the server side
+// check if we still block
+if (hostname === "myhostname") {
+  const blockToday = window.localStorage.getItem("blockToday");
+
+  // allow once
+  const allowOnce = window.localStorage.getItem("allowOnce");
+  if (allowOnce) {
+    // go back before redirect
+  }
+
+  if (blockToday) {
+    // set chrome sync storage accordingly
+  } else {
+    //
+  }
+}
+
+// check block today and let it go if its ok
+// chrome.storage.sync.get(["blockToday"], (result) => {
+//   console.log("codetime value is: ", result.blockToday);
+// });
+*/
+
 chrome.storage.sync.get(["domains"], (res) => {
   domains = res.domains;
 
@@ -25,18 +49,16 @@ chrome.storage.sync.get(["domains"], (res) => {
 
   domains.forEach((domain) => {
     if (hostname.includes(domain)) {
-      document.body.innerHTML = body;
-      document.head.innerHTML = "";
-      console.log(hostname);
+      chrome.runtime.sendMessage({ redirect: "https://google.com" });
     }
   });
 });
 
 // read,edit delete it from content
 
-// chrome.storage.sync.get(['codetime'], (result)=> {
-//   console.log('codetime value is: ', result.codetime)
-// })
+chrome.storage.sync.get(["codetime"], (result) => {
+  console.log("codetime value is: ", result.codetime);
+});
 
 // document.querySelector('#code-time').addEventListener('change', (e) => {
 //   // console.log(e.target.checked)
