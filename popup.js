3832d29bc609e7e23082fbc2f031f1fac73433f0
Yusuf Hilmi <177yusuf177@gmail.com>
2022-06-19 18:08:17 +0300

    add crud for domains

--- popup.js
+++ popup.js
@@ -11,7 +11,7 @@ const handleToggle = (el, habit) => {
   });
 };
 
-const handleEditEnd = (el) => {
+const handleEditHabit = (el) => {
   el.addEventListener("blur", (e) => {
     const habitId = Number(e.target.getAttribute("data-for").substring(5));
     const habit = habits.find((habit) => habit.id === habitId);
@@ -19,6 +19,30 @@ const handleEditEnd = (el) => {
 
     // sync storage
     // chrome.storage.sync.set({ habits: habits });
+  });
+};
+
+const handleEditDomain = (el, id) => {
+  el.addEventListener("blur", (e) => {
+    const domain = domains.find((domain) => domain.id === id);
+
+    domain.host = e.target.textContent;
+
+    console.log(domains);
+    // sync storage
+    // chrome.storage.sync.set({ domains: domains });
+  });
+};
+
+// there is a hidden bug in these indexes, it will change when in use
+const handleDeleteDomain = (el, id) => {
+  el.addEventListener("click", (e) => {
+    const domainIndex = domains.findIndex((domain) => domain.id === id);
+    domains.splice(domainIndex, 1);
+    // sync storage
+    // chrome.storage.sync.set({ domains: domains });
+    console.log(domains);
+    el.parentElement.remove();
   });
 };
 
@@ -42,6 +66,7 @@ const handleDelete = (el) => {
 const domainsList = document.getElementById("domains");
 const habitsList = document.getElementById("habits");
 let habits = [];
+let domains = [];
 
 window.addEventListener("DOMContentLoaded", () => {
   // chrome.storage.sync.get(["habits"], (res) => {
@@ -68,18 +93,42 @@ window.addEventListener("DOMContentLoaded", () => {
     handleToggle(habitListItem.children[0], habit);
 
     // add title update/edit listener
-    handleEditEnd(habitListItem.children[1]);
+    handleEditHabit(habitListItem.children[1]);
 
     // add click listener to remove a habit
     handleDelete(habitListItem.children[2]);
+  });
+  // }); // end of habits sync
+
+  domains = [
+    { id: 1, host: "youtube.com" },
+    { id: 2, host: "instagram.com" },
+    { id: 3, host: "netflix.com" },
+    { id: 4, host: "dizibox" },
+  ];
+
+  domains.forEach((domain, i) => {
+    let domainListItem = document.createElement("li");
+    domainListItem.innerHTML = `
+    <span contenteditable="true">${domain.host}</span> <span>X</span>
+  `;
+    domainsList.append(domainListItem);
+
+    // handle edits
+    handleEditDomain(domainListItem.children[0], domain.id);
+
+    // handle delete
+    handleDeleteDomain(domainListItem.children[1], domain.id);
   });
 });
 
 const addHabitButton = document.getElementById("add-habit");
 
+// add habit
 addHabitButton.addEventListener("click", () => {
   let id;
   if (habits[habits.length - 1]) {
+    // this op is ok since it is ordered but be careful
     id = habits[habits.length - 1].id + 1;
   } else {
     id = 0;
@@ -90,8 +139,8 @@ addHabitButton.addEventListener("click", () => {
     completed: false,
   };
 
-  let newHabit = document.createElement("li");
-  newHabit.innerHTML = `
+  let newHabitListItem = document.createElement("li");
+  newHabitListItem.innerHTML = `
   <input type="checkbox" class="habitToggle" id="habit${id}" ${
     habit.completed ? "checked" : ""
   } /> <span contenteditable="true" data-for="habit${id}">${
@@ -100,16 +149,45 @@ addHabitButton.addEventListener("click", () => {
 
   habits.push(habit);
   // chrome.storage.sync.set({ habits: habits });
-  habitsList.appendChild(newHabit);
+  habitsList.appendChild(newHabitListItem);
 
   console.log("Habit after add: ", habits);
 
   // add toggle listener
-  handleToggle(newHabit.children[0], habit);
+  handleToggle(newHabitListItem.children[0], habit);
 
   // add title update/edit listener
-  handleEditEnd(newHabit.children[1]);
+  handleEditHabit(newHabitListItem.children[1]);
 
   // add click listener to remove a habit
-  handleDelete(newHabit.children[2]);
+  handleDelete(newHabitListItem.children[2]);
+});
+
+const addDomainButton = document.querySelector("#add-domain");
+
+// add new domain to block
+addDomainButton.addEventListener("click", () => {
+  let newHost = "add new";
+  let id;
+  if (domains[domains.length - 1]) {
+    // this op is ok since it is ordered but be careful
+    id = domains[domains.length - 1].id + 1;
+  } else {
+    id = 0;
+  }
+  let newDomainListItem = document.createElement("li");
+  newDomainListItem.innerHTML = `
+  <span contenteditable="true">${newHost}</span> <span>X</span>
+  `;
+
+  domains.push({ id: id, host: newHost });
+  domainsList.appendChild(newDomainListItem);
+
+  console.log("New Domain Added: ", domains);
+
+  // handle edits
+  handleEditDomain(newDomainListItem.children[0], id);
+
+  // handle delete
+  handleDeleteDomain(newDomainListItem.children[1], id);
 });
