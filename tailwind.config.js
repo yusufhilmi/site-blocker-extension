e3d12371e9357436bca775a4e579d01b52b13a6a
Yusuf Hilmi <177yusuf177@gmail.com>
2022-07-09 08:20:31 +0300

    add dark mode to blocked page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      colors: {
+        hab: {
+          900: "#1F1F1F",
+          600: "#6B6B6B",
+          300: "#E6E6E6",
+        },
+      },
       fontFamily: {
         mono: ['"Roboto Mono"'],
       },
